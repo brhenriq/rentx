dd700c468c0c17c923ade0b0c5e4d25921be63a6
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2022-02-16 21:43:55 -0300

    ADD Implement Liskov Substitution Principle

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 
@@ -8,12 +9,9 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const categoryExists = categoriesRepository.findCategoryByName(name);
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  if (categoryExists)
-    return response.json({ message: 'Esta categoria ja existe' });
-
-  categoriesRepository.create({ name, description });
+  createCategoryService.execute({ name, description });
 
   return response.status(201).json();
 });
---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,20 @@
+import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const categoryExists = this.categoriesRepository.findCategoryByName(name);
+
+    if (categoryExists) throw new Error('Category Already Exists!');
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };
