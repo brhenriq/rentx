d2f0b07b58ba460d8e22d20f795320e77582e5d2
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2022-02-18 23:26:55 -0300

    Import File

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listAllCategoriesController } from '../modules/cars/useCases/listAllCategories';
 
+import path from 'path';
+
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: path.join('..', '..', 'tmp'),
+});
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -11,6 +19,10 @@ categoriesRoutes.post('/', (request, response) => {
 
 categoriesRoutes.get('/', (request, response) => {
   return listAllCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
