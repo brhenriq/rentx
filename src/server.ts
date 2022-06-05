203852f42ca60d8ab403d3cee25025965f1ed9df
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2022-06-04 22:54:57 -0300

    FIX: docker-compose

--- src/server.ts
+++ src/server.ts
@@ -13,4 +13,4 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.listen(3333, () => console.log('Server is running'));
+app.listen(3333, () => console.log('Server is running on port 3333 🏁🏆'));
