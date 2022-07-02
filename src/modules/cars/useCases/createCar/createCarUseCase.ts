067fc61b08532f38f67f4f54dbe5db73e8618bfb
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2022-07-02 11:40:49 -0300

    ADD: Create Car test

---
+++ src/modules/cars/useCases/createCar/createCarUseCase.ts
@@ -0,0 +1,45 @@
+import { ICreateCarsDTO } from '@modules/cars/dtos/ICreateCarDTO';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { AppError } from '@shared/errors/AppError';
+import { inject, injectable } from 'tsyringe';
+
+// @injectable()
+class CreateCarUseCase {
+  constructor(
+    // @inject('CarsRepository')
+    private carsRepository: ICarsRepository,
+  ) {}
+
+  async execute({
+    name,
+    brand,
+    daily_rate,
+    description,
+    fine_amount,
+    license_plate,
+    category_id,
+  }: ICreateCarsDTO): Promise<Car> {
+    const carExists = await this.carsRepository.findByLicensePlate(
+      license_plate,
+    );
+
+    if (carExists) {
+      throw new AppError('Car already exists with this license plate');
+    }
+
+    const car = await this.carsRepository.create({
+      name,
+      brand,
+      daily_rate,
+      description,
+      fine_amount,
+      license_plate,
+      category_id,
+    });
+
+    return car;
+  }
+}
+
+export { CreateCarUseCase };
