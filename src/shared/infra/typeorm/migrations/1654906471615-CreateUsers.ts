370d0f52ff507e3eef073f8434515c3ea0be8a08
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2022-07-04 23:03:04 -0300

    ADD: Create Rental migration and FIX Create Users

--- src/shared/infra/typeorm/migrations/1654906471615-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1654906471615-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1654906471615 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
