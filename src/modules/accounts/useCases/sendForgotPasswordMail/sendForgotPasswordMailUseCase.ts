af5fc80d4d792c8bd43fa731ebeb1c991ea32c3d
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2022-07-29 18:06:21 -0300

    ADD: MAil Provider

--- src/modules/accounts/useCases/sendForgotPasswordMail/sendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/sendForgotPasswordMailUseCase.ts
@@ -1,6 +1,7 @@
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
 import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
+import { IMailProvider } from '@shared/container/providers/MailProvider/IMailProvider';
 import { AppError } from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
 import { v4 as uuidv4 } from 'uuid';
@@ -14,9 +15,11 @@ class SendForgotPasswordMailUseCase {
     private usersTokensRepository: IUsersTokensRepository,
     @inject('DateProvider')
     private dateProvider: IDateProvider,
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
   ) {}
 
-  async execute(email: string) {
+  async execute(email: string): Promise<void> {
     const user = await this.usersRepository.findUserByEmail(email);
 
     if (!user) {
@@ -30,6 +33,12 @@ class SendForgotPasswordMailUseCase {
       user_id: user.id,
       expires_date: this.dateProvider.addHours(3),
     });
+
+    await this.mailProvider.sendMail(
+      email,
+      'Recuperação de senha',
+      `O link para reset é ${token}`,
+    );
   }
 }
 
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,4 +3,12 @@ import { container } from 'tsyringe';
 import { DayjsDateProvider } from './DateProvider/implementation/DayjsDateProvider';
 import { IDateProvider } from './DateProvider/IDateProvider';
 
+import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
+import { IMailProvider } from './MailProvider/IMailProvider';
+
 container.registerSingleton<IDateProvider>('DateProvider', DayjsDateProvider);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider(),
+);
