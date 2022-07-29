d7d9d4bcc7a8e377a966f77b088f428bb09bcfc8
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2022-07-29 18:00:43 -0300

    ADD: Forgot Password module

---
+++ src/modules/accounts/useCases/sendForgotMail/sendForgotPasswordMailUseCase.ts
@@ -0,0 +1,36 @@
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
+import { AppError } from '@shared/errors/AppError';
+import { inject, injectable } from 'tsyringe';
+import { v4 as uuidv4 } from 'uuid';
+
+@injectable()
+class SendForgotPasswordMailUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject('DateProvider')
+    private dateProvider: IDateProvider,
+  ) {}
+
+  async execute(email: string) {
+    const user = await this.usersRepository.findUserByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists!');
+    }
+
+    const token = uuidv4();
+
+    await this.usersTokensRepository.create({
+      refresh_token: token,
+      user_id: user.id,
+      expires_date: this.dateProvider.addHours(3),
+    });
+  }
+}
+
+export { SendForgotPasswordMailUseCase };
